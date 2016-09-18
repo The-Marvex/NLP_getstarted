a0724ae46ed9e0986e7db4740f851c9113008218
getsanjeev <getsanjeevdubey@gmail.com>
2016-09-19 00:42:49 +0530

    tokenizer corrected

--- Chat_1.cpp
+++ Chat_1.cpp
@@ -9,6 +9,9 @@ void count_words();
 void count_lines();
 string getdata_string();
 void tokenize_sentence(string sentence);
+vector<int> KMP_implementation(string text_string, string pattern_string) ;
+vector<int> search_substring(string text_string, string pattern_string);
+void get_partial_match_table(string pattern_string, int pattern_len, int pmt[]);
 
 string file_name;
 string data_string;
@@ -17,7 +20,7 @@ string data_string;
 int main()
 {	
     data_string = getdata_string();
-    tokenize_sentence("sanjeev human pankaj neeraj");
+    tokenize_sentence("sanjeev human pankaj neeraj shobhit rddhima anushtha rahul himanshu kjkljh");
 }
 
 void count_words()
@@ -85,45 +88,122 @@ string getdata_string()
 
 void tokenize_sentence(string sentence)
 {
-	cout<<sentence<<endl;
-	vector<string> token_vector;
-	int size_sentence = sentence.size();
-	int i= 0;
+	cout<<sentence<<endl;		
+	int size_sentence = sentence.size();	
 	char tab2[size_sentence+1];	
 	tab2[size_sentence+1] = '\0';
 	strcpy(tab2, sentence.c_str());		
 	int pre_index = 0;
-	int post_index = 0;
-	int j = 0;		
-	while(tab2[i]!='\0')
-	{		
-		if((tab2[i] == ' '))
-		{
+	int post_index = 0;	
+	vector<int> index_vector = KMP_implementation(sentence, " ");	
+	index_vector.push_back(size_sentence-1);
+	cout<<"size of vector is "<<index_vector.size()<<endl;
+	cout<<index_vector[0]<<index_vector[1]<<index_vector[2]<<index_vector[3]<<"vgvd vector is cute"<<endl;
 
-			post_index = i-1;			
-			char temp_array[post_index - pre_index +1];
-			cout<<"the size of token is: "<<(post_index - pre_index +1)<<endl;
-			cout<<"pre_index is"<<pre_index<<endl;
-			cout<<"post_index is"<<post_index<<endl;
-			for(int k = pre_index;k<=post_index;k++)
-			{
-				temp_array[j] = tab2[k];
-				cout<<temp_array[i]<<" ";
-				j++;
-			}
-			string temp_token(temp_array);
-			cout<<"the temptoken is given by: "<<temp_token<<endl;
-			token_vector.push_back(temp_token);
-			pre_index = (i+1);
-		}	
-
-		i++;		
-	}
-
-	cout<<"The tokens are: "<<endl;
-	for(int i = 0;i<token_vector.size();i++)
+	for(int i = 0;i<index_vector.size();)
 	{
-		cout<<token_vector[i]<<endl;		
+		cout<<index_vector[i]<<"  "<<index_vector[i+1] <<endl;
+		i = i+2;
 	}
+
+	string temp;
+
+	string tokens[(index_vector.size())/2];	
+	int p = 0;
+	int m = 0;
+
+
+	while(m<=index_vector.size()/2)
+	{
+		int temp_size = index_vector[m+1] - index_vector[m] + 1;
+		//cout<<" gets size of vector"<<index_vector[m+1]<<" "<<index_vector[m]<<" "<<temp_size<<endl;
+		char arr[temp_size];
+		int start = p;
+		for(int k = 0;k<temp_size;k++)
+		{			
+			arr[k] = tab2[start];
+			start++;
+		}		
+		temp = arr;	
+		cout<<"The token is: "<<temp;	
+		m = m+2;	
+		p = index_vector[m];
+	}
+			
 }
 
+vector<int> KMP_implementation(string text_string, string pattern_string)   //This is implementation of KMP algorithm
+{	vector<int> v;
+	v = search_substring(text_string, pattern_string);
+	return v;
+}
+
+vector<int> search_substring(string text_string, string pattern_string)
+{
+	vector<int> index_vector;
+	index_vector.push_back(0);
+	int pattern_len, text_len, j, i,count;
+	count  = 0;
+	pattern_len = pattern_string.length();
+	text_len = text_string.length();
+	int pmt[pattern_len];
+	j = 0;
+	get_partial_match_table(pattern_string, pattern_len, pmt);
+	i = 0;
+	while (i < text_len)
+	{
+		if (pattern_string[j] == text_string[i])
+		{
+			j = j+1;
+			i = i+1;
+		}
+		if (pattern_len == j)
+		{
+			cout << "Pattern found at : " << (i - j) << endl;
+			index_vector.push_back(i-j-1);
+			index_vector.push_back(i-j+1);
+			count = count +1;
+			j = pmt[j-1];
+		}
+		else if ((i < text_len)&&(pattern_string[j] != text_string[i]))
+		{
+			if ( 0 != j)
+			{
+				j = pmt[j-1];
+			}
+			else
+			{
+				i = i + 1;
+			}
+		}
+	}
+	return index_vector;   //returns no of times a pattern is contained in it.
+}
+
+void get_partial_match_table(string pattern_string, int pattern_len, int pmt[])
+{
+	int j = 0;
+	int i = 1;
+	pmt[0] = 0;
+	while (i < pattern_len)
+	{
+		if (pattern_string[i] == pattern_string[j])
+		{
+			pmt[i] = j+1;
+			j++;
+			i++;
+		}
+		else
+		{
+			if (0 != j)
+			{
+				j = pmt[j - 1];
+			}
+			else
+			{
+				pmt[i] = 0;
+				i++;
+			}
+		}
+	}
+}
